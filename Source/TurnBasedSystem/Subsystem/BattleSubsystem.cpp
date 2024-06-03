29b83b8568228c0f9d74607795dec4e8d280723d
Wait2Late <w82l8.islam@gmail.com>
2024-06-03 16:41:43 +0200

    Starting to implement some ideas

---
+++ Source/TurnBasedSystem/Subsystem/BattleSubsystem.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BattleSubsystem.h"
+
+void UBattleSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	
+	AmountOfEnemies.SetNum(5);
+	CurrentEnemyIndex = 0;
+	
+}
+
+void UBattleSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+}
+
+void UBattleSubsystem::AddEnemies(const int EnemyAmount)
+{
+	for (int i = 0; i < EnemyAmount; ++i)
+		AmountOfEnemies.Add(i);
+
+	if (AmountOfEnemies.Num() > 5)
+		AmountOfEnemies.SetNum(5);
+}
+
---
+++ Source/TurnBasedSystem/Subsystem/BattleSubsystem.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+#include "BattleSubsystem.generated.h"
+
+/**
+ * 
+ */
+
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnEnemyDefeat, int, RemoveEnemy);
+UCLASS()
+class TURNBASEDSYSTEM_API UBattleSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+
+	UPROPERTY(BlueprintAssignable, Category="Battle Event")
+	FOnEnemyDefeat OnEnemyDefeat;
+
+	
+	UFUNCTION(BlueprintCallable)
+	void AddEnemies(int EnemyAmount);
+private:
+	TArray<int> AmountOfEnemies;
+	int CurrentEnemyIndex;
+};
